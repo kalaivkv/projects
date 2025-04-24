5bcfb433e65963fc4c0be0856f0235c0715c8c8d
kalaivanan <kalaivkv24@gmail.com>
2025-04-24 09:20:44 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
-  basePath: '/projects',
-  assetPrefix: '/projects',
 };
 
 module.exports = nextConfig;
